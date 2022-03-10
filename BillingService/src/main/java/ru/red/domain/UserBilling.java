230ca21e5c3cf2ea56ec406e7ddfa61e6d100365
ReDestroyDeR <daniil.schroder@gmail.com>
2022-03-10 22:58:16 +0300

    Add .data annotations to the domain

--- BillingService/src/main/java/ru/red/domain/UserBilling.java
+++ BillingService/src/main/java/ru/red/domain/UserBilling.java
@@ -1,9 +1,13 @@
 package ru.red.domain;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Data
+@Table("billings")
 public class UserBilling {
+    @Id
     private Long id;
     private String email;
     private Integer balance;
